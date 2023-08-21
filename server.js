5c2548bbad4802b3e271a6375ed79eadb098928a
Oguz <oguzkurukaya@gmail.com>
2023-08-21 23:57:55 +0300

    littlefix

--- server.js
+++ server.js
@@ -17,25 +17,20 @@ const usersRouter = require('./libs/modules/user/routes/userRoutes.js')
 app.use('/users', usersRouter)
 
 
-
 app.listen(3000, () => {
     console.log('Server is listening on port 3000')
 })
 
 
 
-
-
-
-
-app.post('/login', (req,res) => {
+app.post('/login', (req, res) => {
     // Authenticate User
     const username = req.body.username;
-    const user = { name: username }
+    const user = {name: username}
 
     const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
 
     res.json({
-        access : process.env.ACCESS_TOKEN_SECRET
+        access: accessToken
     })
 })
